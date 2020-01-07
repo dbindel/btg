8a0248159c33ffbb8f1a6a30c8c45cd8dfd1b994
Cameron Ibrahim <dev@cameronibrahim.com>
2020-01-07 01:14:16 -0800

    Squared Exponential, BoxCox, Model, FunctionPrior are in, rename and reorganize files

--- BayesianTransformedGaussian/src/BayesianTransformedGaussian.jl
+++ BayesianTransformedGaussian/src/BayesianTransformedGaussian.jl
@@ -1,5 +1,17 @@
 module BayesianTransformedGaussian
 
-greet() = print("Hello World!")
+using Reexport
+
+include("functions/Functions.jl")
+
+@reexport using .Functions
+
+include("Model.jl")
+include("Expectation.jl")
+include("Statistics.jl")
+include("Plotting.jl")
+
+export
+    Model
 
 end # module

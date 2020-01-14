01cc9f0c13a0b8b6cb681acef236913c9f3934ed
le-oh <55510932+le-oh@users.noreply.github.com>
2020-01-13 22:58:06 -0500

    Update transforms.jl

--- individual_implementations/btg julia/transforms.jl
+++ individual_implementations/btg julia/transforms.jl
@@ -12,7 +12,36 @@ end
 Derivative of Box-Cox power transformation w.r.t x
 """
 function boxCoxPrime(x, lambda=1)
-    lambda==0 ? x.^(-1) : x.^(lambda-1)
+    lambda==0 ? x.^(-1) : x.^(lambda .-1)
+end
+
+"""
+Second derivative of Box-Cox power transformation w.r.t x
+"""
+function boxCoxPrime2(x, lambda=1)
+    lambda==0 ? -x.^(-2) : (lambda-1)*x.^(lambda-2)
+end
+
+"""
+Derivative of Box-Cox power transformation w.r.t lambda
+"""
+function boxCoxPrime_lambda(x, lambda=1)
+    lambda==0 ? 0 : (lambda * x.^lambda .* log.(x) .- x.^lambda .+ 1)/lambda^2
+end
+
+"""
+Second derivative of Box-Cox power transformation w.r.t lambda
+"""
+function boxCoxPrime_lambda2(x, lambda=1)
+    lambda==0 ? 0 : x.^lambda .* (Base.log.(x)^2)/lambda - 
+    (2*x.^lambda .* Base.log.(x))/lambda^2 + 2*(x.^lambda-1)/lambda^3
+end
+
+"""
+Mixed derivative of Box-Cox power transformation w.r.t lambda and x
+"""
+function boxCoxMixed_lambda_z(x, lambda=1)
+    lambda==0 ? 0 : x.^(lambda-1) .* Base.log.(x) 
 end
 
 """

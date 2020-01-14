13cb852f4020517c0713212c46c4a1b31fd156c7
le-oh <55510932+le-oh@users.noreply.github.com>
2020-01-13 22:59:25 -0500

    Update driver_abalone.jl

--- individual_implementations/btg julia/driver_abalone.jl
+++ individual_implementations/btg julia/driver_abalone.jl
@@ -14,8 +14,7 @@ data = convert(Matrix, df[:,2:8]) #length, diameter, height, whole weight, shuck
 target = convert(Array, df[:, 9]) #age
 
 #pick training points
-#ind = 1:15 
-ind = 50:54
+ind = 1:40
 s = data[ind, :] 
 #X = data[ind, :] 
 #X = ones(length(data))[ind]
@@ -32,23 +31,27 @@ range_theta = [100 300]
 range_lambda = [-3 3]
 
 if false # use this blockcxsanity check + plot data
+    @printf("hi")
     i = 240
     s0 = data[i:i,:] #covariates and coordinates
-    X0  = [1]
-    pdf = model(X, X0, s, s0, boxCox, boxCoxPrime, pθ, pλ, z, range_theta, range_lambda)
-    cdf = z0 ->  int1D(pdf, 0, z0, "3") 
-    @time begin
-        un = cdf(30)
-    end
-    cdfn = x -> cdf(x)/un #normalized cdf
-    pdfn = x -> pdf(x)/un #normalized pdf
-    plt(pdfn, 0, 30)
+    X0  = data[i:i, 1:3]
+    pdff, cdff = model(X, X0, s, s0, boxCox, boxCoxPrime, pθ, pλ, z, range_theta, range_lambda)
+    constant = cdff(30)
+    pdfn = x -> pdff(x)/constant
+    cdfn = x -> cdff(x)/constant 
+    #cdf = z0 ->  int1D(pdf, 0, z0, "3") 
+    #@time begin
+    #un = cdf(30)
+    #end
+    #cdfn = x -> cdf(x)/un #normalized cdf
+    #pdfn = x -> pdf(x)/un #normalized pdf
+    @time begin plt(pdfn, 0, 30) end
     #display(plot!(target[i], seriestype = :vline))
     #med = bisection(x -> cdfn(x)-0.5, 1e-3, 25, 1e-3, 10) 
     #println(med)
 end
 
-if true #cross validation on training set
+if false #cross validation on training set
     _, Xs, Ys = cross_validate(X, s, boxCox, boxCoxPrime, pθ, pλ, z, range_theta, range_lambda, 500, 2, 24)
     display(plot(Xs, Ys, 
     layout = length(z), 
@@ -62,7 +65,25 @@ if true #cross validation on training set
     xtickfont = Plots.font(4, "Courier"), 
     ytickfont = Plots.font(4, "Courier"), 
     lw=0.5))
-    savefig("results//abalone//abalone_cross_validation11.pdf")
+    savefig("results//abalone//abalone_cross_validation13.pdf")
+end
+
+
+if true #delete one group cross validation
+    Xs, Ys = cross_validate_groups(X, s, boxCox, boxCoxPrime, pθ, pλ, z, range_theta, range_lambda, 5, 500, 2, 24)
+    display(plot(Xs, Ys, 
+    layout = length(z), 
+    legend=nothing, 
+    xtickfont = Plots.font(4, "Courier"),
+    ytickfont = Plots.font(4, "Courier"), 
+    lw=0.5))
+    display(plot!(z', layout = length(z), 
+    legend=nothing, 
+    seriestype = :vline, 
+    xtickfont = Plots.font(4, "Courier"), 
+    ytickfont = Plots.font(4, "Courier"), 
+    lw=0.5))
+    savefig("results//abalone//abalone_group_cross_validation4.pdf")
 end
 
 

f1faa5851652f99a710e497c2c3919bc73031e93
alh99 <55510932+alh99@users.noreply.github.com>
2020-01-27 00:03:50 -0500

    driver for Gauss Turan Quadrature

---
+++ individual_implementations/driver_turan.jl
@@ -0,0 +1,16 @@
+using DataFrames
+using CSV
+
+df = DataFrame(CSV.File("nodes_weights.csv"))
+nodes = convert(Matrix, df[:,1]) #integration nodes for Gauss-Turan Quadrature
+weights = convert(Array, df[:, 2:end]) #integration weights 
+
+function eval_Gauss_Turan(f, df, df2, nodes, weights)
+    fn = f.(nodes)
+    dfn = df.(nodes)
+    df2n = df2.(nodes)
+    return fn'*weights(:, 1) + dfn'*weights(:, 2)+df2n'*weights(:, 3)
+end
+        
+
+

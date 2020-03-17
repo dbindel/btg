4a0cfe56418d09b0cd9bca6f45d55145abc63320
alh99 <55510932+alh99@users.noreply.github.com>
2020-03-17 16:39:25 -0400

    Update plotting.jl

--- BayesTransformedGaussian/src/tools/plotting.jl
+++ BayesTransformedGaussian/src/tools/plotting.jl
@@ -1,1 +1,29 @@
+"""
+Generates data for plotting f on [a, b]
+"""
+function plt_data(f, a, b, numpts=1000)
+    h = (b-a)/(numpts-1)
+    y = zeros(1,numpts)
+    x = collect(a:h:b)
+    y = map(f, x)
+    return x, y
+end
 
+"""
+Displays graph of f on interval [a, b]
+"""
+function plt(f, a, b, numpts=100)
+    h = (b-a)/(numpts-1)
+    y = zeros(1,numpts)
+    x = collect(a:h:b)
+    y = map(f, x)
+    display(plot(x, y))
+end
+
+function plt!(f, a, b, numpts=100)
+    h = (b-a)/(numpts-1)
+    y = zeros(1,numpts)
+    x = collect(a:h:b)
+    y = map(f, x)
+    display(plot!(x, y))
+end

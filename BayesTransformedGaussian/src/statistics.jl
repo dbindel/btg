cf043491ca30d08e82767d004295fb001180e32f
Xinran Zhu <xz584@cornell.edu>
2020-03-19 18:06:11 -0400

    update CI computation

--- BayesTransformedGaussian/src/statistics.jl
+++ BayesTransformedGaussian/src/statistics.jl
@@ -69,144 +69,113 @@ function credible_interval(btg::BTG, x::AbstractVector{R}, wp::R, ::Val{:equal})
 end
 
 function credible_interval(btg::BTG, x::AbstractVector{R}, wp::R, ::Val{:narrow}) where R <: Real
-  #= Notations:
-  [alpha, beta]: 
-      interval for integration, target credible interval
-      s.t. 1) pdf(alpha) = pdf(beta) and 2) integral of pdf from alpha to beta = wp (like 0.95)
-  l_height: 
-    value of pdf at some point, height of current l line
-  l_fun_equal = abs(l(x) - l_height): 
-      minimize this absolute value is to find another point s.t. l(x) = l_height 
-      i.e. given a height of the l line, find an intersection of the horizontal l line and pdf curve
-  hquadrature: 
-      function from the package Cubature for numerical integration
-      e.g. integral_heightue, error = hquadrature(f, a, b) gives integral of f on [a,b] with some error
-      we mostly use this to compute integral of pdf on [alpha, beta] for different alpha and beta
-  l_height_low/l_height_high: 
-      height of an l line that is lower/higher than the target one
-  alpha_low/alpha_high, beta_low/beta_high: 
-      the corresponding interval
-
-  =#
-
   #= 
   Brief idea: bisection
     Suppose the target interval is [alpha*, beta*], i.e. integral of pdf on [alpha*, beta*] = wp
     and the corresponding height is pdf(alpha*) = pdf(beta*) = l_height*
-    We call the horizontal line passing through pdf(alpha*) and pdf(beta*) the target l line, l*
-    We first find two horizontal l lines sitting on each side of l*
-    and they are called l_height_low and l_high
+    Say l* is the horizontal line intersecting with pdf curve at alpha* and beta*, 
+    we first find two horizontal l lines sitting on each side of l*
+    One is lower with height l_height_low, and one is higher with height l_height_high
     Then we use a bisection way to gradually find the target l*
   =#
-
-  #= 
-  Step 1: 
-    find an initial interval [alpha, beta] to start
-    such that pdf(alpha) = pdf(beta), i.e. same height
-    also compute the integral of pdf on current [alpha, beta] 
+     
+  #= Notations:
+  [alpha, beta]: 
+      interval for integration, target credible interval
+      s.t. 1) pdf(alpha) = pdf(beta) and 2) integral of pdf from alpha to beta = wp (like 0.95)
+  l_height_low/l_height_high: 
+      height of the l line that is lower/higher than the target one
+  alpha_low/alpha_high, beta_low/beta_high: 
+      the corresponding interval
+  int_low/int_high: 
+      the corresponding integral value, i.e. integral of pdf on [alpha_low/high, beta_low/high]
+  hquadrature: 
+      function from the package Cubature for numerical integration
+      e.g. integral_heightue, error = hquadrature(f, a, b) gives integral of f on [a,b] with some error
+      we mostly use this to compute integral of pdf on [alpha, beta] for different alpha and beta
   =#
-  median = quantile(btg, x, p=0.5)
-  # set initial beta to be a little larger than median, no specific reason, just a start
-  beta = 1.1 * median
-  l_height =  pdf(btg, beta)
-  l_fun_equal(x) = abs(pdf(btg, x) - l_height)
-  # find alpha such that pdf(alpha) = pdf(beta)
-  routine = optimize(l_fun_equal, 1e-10, median) 
-  alpha = Optim.minimizer(routine)
-  # compute the integral of pdf on [alpha,beta]
-  int = hquadrature(x -> pdf(btg, x), alpha, beta)[1]
+   
+  # assume we have the mode
+  mode_d = mode(btg, x)
+  l_height_low = 1e-3
+  α_low, β_low, int_low = find_αβ(l_height_low, [0, mode_d], [mode_d, bound])
+  # adjust height if l low is not lower than l* (i.e. int < wp)
+  l_height_low, α_low, β_low, int_low = adjust(l_height_low, α_low, β_low, int_low, "low")
 
-  #= 
-  Step 2: 
-    find another interval [alpha, beta] 
-    s.t. this l line and the initial l line found in step 1 are on different side of the target l line
-    where the target l line means the credible interval we want
-  =#
+  l_height_high = f(mode_d) - 1e-6
+  α_high, β_high, int_high = find_αβ(l_height_high, [0, mode_d], [mode_d, bound])
+  # adjust height if l_high is not higher than l* (i.e. int > wp)
+  l_height_high, α_high, β_high, int_high = adjust(l_height_high, α_high, β_high, int_high, "high")
 
-  if int < wp
-    # int < wp means the interval is narrower than [alpha*, beta*] 
-    # so it means that the line is higher than the target line l*
-    while int < wp
-        l_height_up = l_height
-        alpha_up = alpha
-        beta_up = beta
-        int_up = int
-        
-        # if the current l line is higher than the target, 
-        # we lower it until its lower than the target line
-        l_height *= 0.6
-        routine_alpha = optimize(l_fun_equal, 1e-10, alpha) 
-        alpha = Optim.minimizer(routine_alpha)
-        routine_beta = optimize(l_fun_equal, beta, b)
-        beta = Optim.minimizer(routine_beta)
-        int = hquadrature(x -> pdf(btg, x), alpha, beta)[1]
-        println("Current tuple [$alpha, $beta, $l_height, $int]")
-    end
-    l_height_low = l_height
-    alpha_low = alpha
-    beta_low = beta
-    int_low = int
-  else
-    # int > wp means the interval is larger than [alpha*, beta*] 
-    # so it means that the line is lower than the target line l*
-    while int > wp
-      l_height_low = l_height
-      alpha_low = alpha
-      beta_low = beta
-      int_low = int
-      
-      # if the current l line is higher than the target, 
-      # we lower it until its lower than the target line
-      l_height *= 2
-      routine_alpha = optimize(l_fun_equal, alpha, mean_temp) 
-      alpha = Optim.minimizer(routine_alpha)
-      routine_beta = optimize(l_fun_equal, mean_temp, beta)
-      beta = Optim.minimizer(routine_beta)
-      int = hquadrature(x -> pdf(btg, x), alpha, beta)[1]
-      l_height_low = l_height
-      alpha_low = alpha
-      beta_low = beta
-      int_low = int
-    end
-    l_height_up = l_height
-    alpha_up = alpha
-    beta_up = beta
-    int_up = int
-  end
-
-
-  # Step 3: Bisection to find a middle l line, intersecting with pdf at alpha and beta 
-  # s.t. integral of pdf on [alpha, beta] = wp 
+  α_mid = 0.
+  β_mid = 0.
   l_mid = 0.
-  alpha_mid = 0.
-  beta_mid = 0.
   N = 0
+  int = int_low
+  while !isapprox(int, wp) && N < 50
+  l_mid = (l_height_high + l_height_low)/2
+  α_mid, β_mid, int = find_αβ(l_mid, [α_low, α_high], [β_high, β_low])
+  if int > wp
+      l_height_low = l_mid
+      α_low = α_mid
+      β_low = β_mid
+  else
+      l_height_high = l_mid
+      α_high = α_mid
+      β_high = β_mid
+  end
+  N += 1
+  end
+  return [α_mid, β_mid, l_mid]
 
-  while !isapprox(int, wp) && N < 100
-    l_mid = (l_height_up + l_height_low)/2
-    alpha_mid = Optim.minimizer(optimize(x -> abs(l_fun(x) - l_mid), alpha_low, alpha_up))
-    beta_mid = Optim.minimizer(optimize(x -> abs(l_fun(x) - l_mid), beta_up, beta_low))
-    int = hquadrature(l_fun, alpha_mid, beta_mid)[1]
-    if int > wp
-        l_height_low = l_mid
-        alpha_low = alpha_mid
-        beta_low = beta_mid
-    else
-        l_height_up = l_mid
-        alpha_up = alpha_mid
-        beta_up = beta_mid
-    end
-    N += 1
+  # helper functions
+  #= 
+  given a height l_height, find the two intersections s.t. pdf(α) = pdf(β) = l_height
+  Input:
+    l_height: given height
+    α_intvl/β_intvl: interval for root finding for alpha/beta
+  Output:
+    [α, β]: the two intersections
+    int: integral value of pdf on [α, β]
+  =#
+  function find_αβ(l_height, α_intvl, β_intvl)
+    # find α and β within ginve intervals α_intvl and β_intvl respectively
+    routine_α = optimize(x -> abs(f(x) - l_height), α_intvl[1], α_intvl[2], GoldenSection())
+    α = Optim.minimizer(routine_α)
+    routine_β = optimize(x -> abs(f(x) - l_height), β_intvl[1], β_intvl[2], GoldenSection())
+    β = Optim.minimizer(routine_β)
+    int = hquadrature(f, α, β)[1]
+    return α, β, int
   end
 
-  # assert the constraint pdf(alpha) = pdf(beta)
-  @assert isapprox(l_fun(alpha_mid), l_fun(beta_mid)) "Should have equal value of pdf function."
-  # assert integral of pdf on [alpha, beta] = wp
-  @assert isapprox(int, wp) "Should have $wp confidence interval"
-
-  # return an array storing height of l and the credible interval [alpha_mid, beta_mid] found by bisection
-  CI = [l_mid, alpha_mid, beta_mid]  
-
+  #= 
+  Adjust the height in case initial choice of low/high is not proper 
+    Since we want int_low > wp and int_high < wp to do bisection, 
+    we have to adjust height if initial int_low < wp or int_high > wp
+  Input:
+    l: current height
+    [α, β]: current interval
+    int: current integral value
+    MODE: indicates we are adjusting the low line or the high line
+  Output:
+    According values after adjustment
+    s.t. int > wp if MODE == "low"; int < wp else.
+ =#
+  function adjust(l, α, β, int, MODE)
+    # adjust if int < wp
+    if MODE == "low"
+        while int < wp
+            l /= 2
+            α, β, int = find_αβ(l, [0, α], [β, bound])
+        end
+    else
+        while int > wp
+            l = (l + f(mode_d))/2
+            α, β, int = find_αβ(l, [α, mode_d], [mode_d, β])
+        end
+    end
+    return l, α, β, int
+  end
 end
     
 end

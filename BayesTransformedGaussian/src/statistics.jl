049045f8c9efafcc20ad71d38524b78264cb0d46
Xinran Zhu <xz584@cornell.edu>
2020-03-21 19:58:51 -0400

    update walkthrough

--- BayesTransformedGaussian/src/statistics.jl
+++ BayesTransformedGaussian/src/statistics.jl
@@ -4,52 +4,43 @@ using Cubature
 # import Statistics: median, pdf, cdf
 
 function median(pdf, cdf; pdf_deriv=nothing)
-    med = quantile(pdf, cdf, pdf_deriv)
+    med = quantile(pdf, cdf)
     return med
 end
 
 function quantile(pdf, cdf; pdf_deriv=nothing, p::R=.5) where R <: Real
-    quantile_func(x) = cdf(x) - p
-
-    function quantile_deriv!(storage, x)
-        storage[1] = pdf(x)
-    end
-
-    initial_guess = [ rand() ]
-    routine = optimize(quantile_func, quantile_deriv!, initial_guess, BFGS())
-
+    quantile_func(x) = abs(cdf(x) - p)
+    # derivative
+    # function quantile_deriv!(storage, x)
+    #     storage[1] = pdf(x)
+    # end
+    routine = optimize(quantile_func, 0., 3.)
     quant = Optim.minimizer(routine)
-
     return quant
 end
 
 function mode(pdf, cdf; pdf_deriv=nothing) 
     # maximize the pdf
-    initial_guess = [ rand() ]
-    function mode_deriv!(storage, x)
-        storage[1] = - pdf_deriv(x)
-    end
-    routine = optimize(x -> -pdf(x), mode_deriv!, initial_guess, BFGS())
-
-    mod = Optim.minimizer()
+    routine = optimize(x -> -pdf(x), 0., 3.)
+    mod = Optim.minimizer(routine)
     return mod
 end
 
 @doc raw"""
 """
-function credible_interval(pdf, cdf; pdf_deriv=nothing, wp=.95::R, mode=:narrow) where R <: Real
-    return credible_interval(pdf, cdf, pdf_deriv, wp, Val(mode))
+function credible_interval(pdf, cdf; pdf_deriv=nothing, wp::R=.95, mode=:equal) where R <: Real
+    return credible_interval(pdf, cdf, Val(mode); pdf_deriv=pdf_deriv, wp=wp)
 end
 
-function credible_interval(pdf, cdf, ::Val{:equal}; pdf_deriv=nothing, wp=.95::R) where R <: Real
+function credible_interval(pdf, cdf, ::Val{:equal}; pdf_deriv=nothing, wp::R=.95) where R <: Real
     lower_qp = (1 - wp) / 2
     upper_qp = 1 - lower_qp
-    lower_quant = quantile(btg, x, p=lower_qp)
-    upper_quant = quantile(btg, x, p=upper_qp)
+    lower_quant = quantile(pdf, cdf; p=lower_qp)
+    upper_quant = quantile(pdf, cdf; p=upper_qp)
     return [lower_quant, upper_quant]
 end
 
-function credible_interval(pdf, cdf, ::Val{:narrow}; pdf_deriv=nothing, wp=.95::R) where R <: Real
+function credible_interval(pdf, cdf, ::Val{:narrow}; pdf_deriv=nothing, wp::R=.95) where R <: Real
   #= 
   Brief idea: bisection
     Suppose the target interval is [alpha*, beta*], i.e. integral of pdf on [alpha*, beta*] = wp
@@ -79,43 +70,7 @@ function credible_interval(pdf, cdf, ::Val{:narrow}; pdf_deriv=nothing, wp=.95::
       we mostly use this to compute integral of pdf on [alpha, beta] for different alpha and beta
   =#
    
-  # assume we have the mode
-  mode_d = mode(pdf, cdf, pdf_deriv)
-  # z normalized to [0,1], so reasonably pdf(10) ~= 0
-  bound = 5.
-  l_height_low = 1e-3
-  α_low, β_low, int_low = find_αβ(l_height_low, [0, mode_d], [mode_d, bound])
-  # adjust height if l low is not lower than l* (i.e. int < wp)
-  l_height_low, α_low, β_low, int_low = adjust(l_height_low, α_low, β_low, int_low, "low")
-
-
-  l_height_high = pdf(btg, mode_d) - 1e-6
-  α_high, β_high, int_high = find_αβ(l_height_high, [0, mode_d], [mode_d, bound])
-  # adjust height if l_high is not higher than l* (i.e. int > wp)
-  l_height_high, α_high, β_high, int_high = adjust(l_height_high, α_high, β_high, int_high, "high")
-
-  α_mid = 0.
-  β_mid = 0.
-  l_mid = 0.
-  N = 0
-  int = int_low
-  while !isapprox(int, wp) && N < 50
-  l_mid = (l_height_high + l_height_low)/2
-  α_mid, β_mid, int = find_αβ(l_mid, [α_low, α_high], [β_high, β_low])
-  if int > wp
-      l_height_low = l_mid
-      α_low = α_mid
-      β_low = β_mid
-  else
-      l_height_high = l_mid
-      α_high = α_mid
-      β_high = β_mid
-  end
-  N += 1
-  end
-  return [α_mid, β_mid, l_mid]
-
-  # helper functions
+ # helper functions
   #= 
   given a height l_height, find the two intersections s.t. pdf(α) = pdf(β) = l_height
   Input:
@@ -127,11 +82,11 @@ function credible_interval(pdf, cdf, ::Val{:narrow}; pdf_deriv=nothing, wp=.95::
   =#
   function find_αβ(l_height, α_intvl, β_intvl)
     # find α and β within ginve intervals α_intvl and β_intvl respectively
-    routine_α = optimize(x -> abs(pdf(btg, x) - l_height), α_intvl[1], α_intvl[2], GoldenSection())
+    routine_α = optimize(x -> abs(pdf(x) - l_height), α_intvl[1], α_intvl[2], GoldenSection())
     α = Optim.minimizer(routine_α)
-    routine_β = optimize(x -> abs(pdf(btg, x) - l_height), β_intvl[1], β_intvl[2], GoldenSection())
+    routine_β = optimize(x -> abs(pdf(x) - l_height), β_intvl[1], β_intvl[2], GoldenSection())
     β = Optim.minimizer(routine_β)
-    int = hquadrature(x -> pdf(btg, x), α, β)[1]
+    int = hquadrature(x -> pdf(x), α, β)[1]
     return α, β, int
   end
 
@@ -157,12 +112,50 @@ function credible_interval(pdf, cdf, ::Val{:narrow}; pdf_deriv=nothing, wp=.95::
         end
     else
         while int > wp
-            l = (l + pdf(btg, mode_d))/2
+            l = (l + pdf(mode_d))/2
             α, β, int = find_αβ(l, [α, mode_d], [mode_d, β])
         end
     end
     return l, α, β, int
   end
+
+  # assume we have the mode
+  mode_d = mode(pdf, cdf)
+  # z normalized to [0,1], so reasonably pdf(10) ~= 0
+  bound = 5.
+  l_height_low = 1e-3
+  α_low, β_low, int_low = find_αβ(l_height_low, [0, mode_d], [mode_d, bound])
+  # adjust height if l low is not lower than l* (i.e. int < wp)
+  l_height_low, α_low, β_low, int_low = adjust(l_height_low, α_low, β_low, int_low, "low")
+
+
+  l_height_high = pdf(mode_d) - 1e-6
+  α_high, β_high, int_high = find_αβ(l_height_high, [0, mode_d], [mode_d, bound])
+  # adjust height if l_high is not higher than l* (i.e. int > wp)
+  l_height_high, α_high, β_high, int_high = adjust(l_height_high, α_high, β_high, int_high, "high")
+
+  α_mid = 0.
+  β_mid = 0.
+  l_mid = 0.
+  N = 0
+  int = int_low
+  while !isapprox(int, wp) && N < 50
+  l_mid = (l_height_high + l_height_low)/2
+  α_mid, β_mid, int = find_αβ(l_mid, [α_low, α_high], [β_high, β_low])
+  if int > wp
+      l_height_low = l_mid
+      α_low = α_mid
+      β_low = β_mid
+  else
+      l_height_high = l_mid
+      α_high = α_mid
+      β_high = β_mid
+  end
+  N += 1
+  end
+  return [α_mid, β_mid]
+
+ 
 end
     
 

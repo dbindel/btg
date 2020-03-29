782a892f23ab9724d28c952ec419660ccebf18f5
Cameron Ibrahim <dev@cameronibrahim.com>
2020-03-29 04:44:31 -0700

    Update incremental data structures to have a more common interface

--- BayesTransformedGaussian/src/bayesopt/incremental.jl
+++ BayesTransformedGaussian/src/bayesopt/incremental.jl
@@ -10,51 +10,38 @@ import LinearAlgebra: ldiv!, det, logdet, isposdef
 @doc raw"""
 """
 mutable struct IncrementalCholesky{T} <: Factorization{T}
-    capacity::Int
     n::Int
     info::Int
     R::Matrix{T}
 end
-function incremental_cholesky(capacity, c::Number; check=true)
-    info = 0
-    if c <= 0
-        check && throw(LinearAlgebra.PosDefException(1))
-        info = -1
-    end
-    R = Array{typeof(sqrt(c))}(undef, capacity, capacity)
-    R[1, 1] = sqrt(c)
-    return IncrementalCholesky(capacity, 1, info, R)
-end
-function incremental_cholesky(capacity, A::AbstractMatrix; check=true)
-    n = size(A, 1)
-    R = Array{eltype(A)}(undef, capacity, capacity)
-    R[1:n, 1:n] .= A
-    chol = cholesky!(Symmetric(@view R[1:n, 1:n]); check=check)
-    return IncrementalCholesky(capacity, n, chol.info, R)
-end
-
-function incremental_cholesky!(n, A::AbstractMatrix; check=true)
+function incremental_cholesky!(A::AbstractMatrix, n; check=true)
     @assert size(A, 1) == size(A, 2)
     @views chol = cholesky!(Symmetric(A[1:n, 1:n]); check=check)
-    return IncrementalCholesky(size(A, 1), n, chol.info, A)
+    return IncrementalCholesky(n, chol.info, A)
+end
+function incremental_cholesky(T, capacity)
+    return incremental_cholesky!(Array{T}(undef, capacity, capacity), 0)
 end
 
-function add_col!(R::IncrementalCholesky, v; check=true)
-    @assert R.n + 1 <= R.capacity
-    n = R.n
-    @views R.R[1:n, n+1] = get_chol(R).L \ v[1:end-1]
-    @views R.R[n+1, n+1] = sqrt(v[end] - dot(R.R[1:n, n+1], R.R[1:n, n+1]))
-    if R.R[n+1, n+1] <= 0
-        check && throw(LinearAlgebra.PosDefException(n+1))
-        R.info = -1
-    end
-    R.n += 1
+function extend!(R::IncrementalCholesky, k)
+    @assert R.n + k <= size(R.R, 1)
+    return @views R.R[1:R.n, R.n+1:R.n+k], R.R[R.n+1:R.n+k, R.n+1:R.n+k]
+end
+
+function update!(R::IncrementalCholesky, k; check=true)
+    @assert R.n + k <= size(R.R, 1)
+    @views A12, A2 = R.R[1:R.n, R.n+1:R.n+k], R.R[R.n+1:R.n+k, R.n+1:R.n+k]
+    ldiv!(get_chol(R).L, A12)
+    mul!(A2, A12', A12, -1, 1)
+    chol = cholesky!(Symmetric(A2); check=check)
+    R.info = chol.info
+    R.n += k
     return nothing
 end
 
-function remove_col!(R::IncrementalCholesky)
-    @assert R.n - 1 >= 1
-    R.n -= 1
+function remove!(R::IncrementalCholesky, k)
+    @assert R.n - k >= 1
+    R.n -= k
     return nothing
 end
 
@@ -77,26 +64,15 @@ isposdef(R::IncrementalCholesky) = R.info == 0
 ##### 
 
 mutable struct DataArray{T,N} <: AbstractArray{T,N}
-    capacity::Int
     n::Int
     A::Array{T,N}
 end
-function data_array(T, capacity, dims::Vararg{Int, N}) where N
-    n = length(dims)
-    A = Array{T, N+1}(undef, dims..., capacity)
-    return DataArray(capacity, 0, A)
-end
-function data_array(capacity, A::Array{T, N}) where T where N
-    s = size(A)
-    new_A = Array{T, N}(undef, ntuple(i -> s[i], N-1)..., capacity)
-    new_A[ntuple(i -> 1:s[i], N)...] .= A
-    return DataArray(capacity, s[N], new_A)
-end
 
-data_array!(n, A::Array{T, N}) where T where N = DataArray(size(A)[end], n, A)
+data_array(T, capacity, dims...) = DataArray(0, Array{T}(undef, dims..., capacity))
+data_array(A::Array, n) = DataArray(n, A)
 
-function array_view(A::DataArray{T, N}) where T where N
-    return view(A.A, ntuple(_ -> Colon(), N-1)..., 1:A.n)
+function array_view(A::DataArray)
+    return view(A.A, ntuple(_ -> Colon(), ndims(A) - 1)..., 1:A.n)
 end
 
 size(A::DataArray, args...) = size(array_view(A), args...)
@@ -105,31 +81,20 @@ getindex(A::DataArray, args...) = getindex(array_view(A), args...)
 
 setindex!(A::DataArray, args...) = setindex!(array_view(A), args...)
 
-function extend!(A::DataArray{T, N}, m) where T where N
-    @assert A.n + m <= A.capacity
-    vw = view(A.A, :, A.n+1:A.n+m)
-    A.n += m
-    return vw
+function extend!(A::DataArray, k)
+    @assert A.n + k <= size(A.A)[end]
+    return view(A.A, :, A.n+1:A.n+k)
 end
 
-function add_point!(A::DataArray{T, N}, v) where T where N
-    @assert A.n + 1 <= A.capacity
-    A.A[ntuple(_ -> Colon(), N-1)..., A.n + 1] .= v
-    A.n += 1
+function update!(A::DataArray, k)
+    @assert A.n + k <= size(A)[end]
+    A.n += k
     return nothing
 end
 
-function add_points!(A::DataArray{T, N}, B) where T where N
-    m = size(B)[end]
-    @assert A.n + m <= A.capacity
-    A.A[ntuple(_ -> Colon(), N-1)..., A.n:A.n+m] .= B
-    A.n += m
-    return nothing
-end
-
-function remove_point!(A::DataArray)
-    @assert A.n - 1 >= 1
-    A.n -= 1
+function remove!(A::DataArray, k)
+    @assert A.n - k >= 1
+    A.n -= k
     return nothing
 end
 

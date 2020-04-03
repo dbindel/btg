f10953e8beb15cf48811f44dc79b90371f9bbd87
Xinran Zhu <xz584@cornell.edu>
2020-04-03 00:59:44 -0400

    update quantile computation

--- BayesTransformedGaussian/src/model0.jl
+++ BayesTransformedGaussian/src/model0.jl
@@ -69,7 +69,7 @@ end
 
 function solve(btg::btg)
     weightTensorGrid = weight_comp(btg)
-    (pdf, cdf, dpdf) = prediction_comp(btg, weightTensorGrid)
+    (pdf, cdf, dpdf，TmixInfo) = prediction_comp(btg, weightTensorGrid)
 end
 
 """

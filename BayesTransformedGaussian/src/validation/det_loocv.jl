f094b03b5ed99056f2ba42193e472a5dfa978cc0
Leo Huang <albert11813@gmail.com>
2020-04-13 01:24:19 -0400

    tests for logdet_loocv

--- BayesTransformedGaussian/src/validation/det_loocv.jl
+++ BayesTransformedGaussian/src/validation/det_loocv.jl
@@ -21,11 +21,34 @@ function det_loocv(choleskyΣθ, detcholeskyΣθ::Float64, i::Int64)
 end
 
 """
+compute determinant of submatrix of Σθ formed by deleting ith row and column efficiently
+given determinant of full matrix
+"""
+#function det_loocv(holeskyΣθ::IncrementalCholeskyc, detcholeskyΣθ::Float64, i::Int64)
+function det_loocv_IC(choleskyΣθ::IncrementalCholesky, detcholeskyΣθ::Float64, i::Int64)
+    n = size(choleskyΣθ)[1]
+    #println("i: ", i)
+    @assert i >= 1 && n >= i
+    #R = get_chol(choleskyΣθ)
+    L = get_chol(choleskyΣθ).L
+    return detcholeskyΣθ * norm(L\ unitvec(n, i))^2
+end
+
+"""
 stably compute log determinant of submatrix of Σθ formed by deleting ith row and column efficiently
 given determinant of full matrix
 """
 #function det_loocv(holeskyΣθ::IncrementalCholeskyc, detcholeskyΣθ::Float64, i::Int64)
-function logdet_loocv(choleskyΣθ, logdetcholeskyΣθ::Float64, i::Int64)
+function logdet_loocv_IC(choleskyΣθ::IncrementalCholesky, logdetcholeskyΣθ::Float64, i::Int64)
+    n = size(choleskyΣθ)[1]
+    #println("i: ", i)
+    @assert i >= 1 && n >= i
+    #R = get_chol(choleskyΣθ)
+    L = get_chol(choleskyΣθ).L
+    return logdetcholeskyΣθ + 2*log(norm(L\ unitvec(n, i)))
+end
+
+function logdet_loocv(choleskyΣθ::Cholesky{Float64,Array{Float64,2}}, logdetcholeskyΣθ::Float64, i::Int64)
     n = size(choleskyΣθ)[1]
     #println("i: ", i)
     @assert i >= 1 && n >= i
@@ -55,6 +78,27 @@ function det_XΣX_loocv(X, choleskyΣθ, detcholeskyΣθ::Float64, i)
 end
 
 
+"""
+Compute determinant of submatrix of - [Sigmainv X; X' 0]. IC stands for IncrementalCholesky
+"""
+function det_XΣX_loocv_IC(X, choleskyΣθ::IncrementalCholesky, detcholeskyΣθ::Float64, i)
+    n = size(choleskyΣθ, 1)
+    p = size(X, 2)
+    @assert n == size(X, 1)
+    @assert i >=1 && n>=i 
+    chol = get_chol(choleskyΣθ)
+    ei = unitvec(n, i)
+    #R11 = choleskyΣθ.U #upper triangular factor
+    R12 = get_chol(choleskyΣθ).L\X
+    cholR22 = cholesky(R12'*R12)
+    a = get_chol(choleskyΣθ).L\ei
+    b = - (cholR22.L \ (R12'*a))
+    eWe =  norm(a)^2 - norm(b)^2
+    detW = (-1)^n * detcholeskyΣθ * det(cholR22)
+    det_Σθ_minus_i = det_loocv_IC(choleskyΣθ, detcholeskyΣθ, i)
+    return eWe * detW / det_Σθ_minus_i
+end
+
 
 """
 Stably compute log of absolute value of determinant of submatrix of - [Sigmainv X; X' 0], where
@@ -81,5 +125,28 @@ function logdet_XΣX_loocv(X, choleskyΣθ, logdetcholeskyΣθ::Float64, i)
     logdetW = logdetcholeskyΣθ + logdet(cholR22)
     #println("logdetW: ", logdetW)
     logdet_Σθ_minus_i = logdet_loocv(choleskyΣθ, logdetcholeskyΣθ, i) #submatrix of PD matrix is PD
+    return logeWe + logdetW - logdet_Σθ_minus_i
+end
+
+function logdet_XΣX_loocv_IC(X, choleskyΣθ::IncrementalCholesky, logdetcholeskyΣθ::Float64, i)
+    n = size(choleskyΣθ)[1]
+    p = size(X, 2)
+    @assert n == size(X, 1)
+    @assert i >=1 && n>=i 
+    ei = unitvec(n, i)
+    #R11 = choleskyΣθ.U #upper triangular factor
+    R12 = get_chol(choleskyΣθ).L\X
+    cholR22 = cholesky(R12'*R12)
+    a = get_chol(choleskyΣθ).L\ei
+    b = - (cholR22.L \ (R12'*a))
+    #println("norm a squared: ", norm(a)^2)
+    #println("norm b squared: ", norm(b)^2)
+    logeWe =  log(abs(norm(a)^2 - norm(b)^2))
+    #println("logeWe: ", logeWe)
+    #logdetW = (-1)^n * detcholeskyΣθ * det(cholR22)
+    #println("logdet(cholR22): ", logdet(cholR22))
+    logdetW = logdetcholeskyΣθ + logdet(cholR22)
+    #println("logdetW: ", logdetW)
+    logdet_Σθ_minus_i = logdet_loocv_IC(choleskyΣθ, logdetcholeskyΣθ, i) #submatrix of PD matrix is PD
     return logeWe + logdetW - logdet_Σθ_minus_i
 end
--- BayesTransformedGaussian/src/validation/test_det_loocv.jl
+++ BayesTransformedGaussian/src/validation/test_det_loocv.jl
@@ -1,4 +1,5 @@
 include("det_loocv.jl")
+using Test
 
 #####
 ##### test det_loocv
@@ -6,26 +7,37 @@ include("det_loocv.jl")
 #the inremental_cholesky is just a wrapper type that allows one to easily extend the cholesky
 #decomposition without allocation more space at each iteration, because a fixed amount of space 
 #is allocated at the beginning, and what you get to use is a view of the top left n x n corner
-
+#println("test det_loocv.jl...")
 n=5
-A = rand(n, n); A = A'*A + UniformScaling(1.0)
+A = rand(n, n); A = A'*A + UniformScaling(4.0)
 #IC = incremental_cholesky!(A, n-1) #make it n-1 just for kicks
 IC = cholesky(A)
+
+vals = zeros(1, n)
+vals1 = zeros(1, n)
 for i=1:n
     subdet = det(A[[1:i-1;i+1:end], [1:i-1;i+1:end]]) 
-    println("actual subdets $i: ", subdet)
+    vals[i] = subdet
+    #println("actual subdets $i: ", subdet)
 end
 
 det_original = det(IC)
 for i = 1:n
     subdet = det_loocv(IC, det_original, i)
-    println("fast subdets $i: ", subdet)
+    vals1[i] = subdet
+    #println("fast subdets $i: ", subdet)
+end
+
+@testset "det_loocv.jl" begin
+    @test norm(vals-vals1)<1e-10
 end
 
 #####
 ##### test det_XΣX_loocv
 #####
-n = 5
+#println("test det_XΣX_loocv...")
+
+n = 20
 m = 3
 X = rand(n, m)
 A = (w = rand(n, n);w'*w + UniformScaling(5))
@@ -35,38 +47,82 @@ bigmat = [A X; X' zeros(m, m)]
 #for i = 1:n
 #    println("actual subdet $i: ", det(bigmat[[1:i-1;i+1:end], [1:i-1;i+1:end]]))
 #end
+vals = zeros(1, n)
+vals1 = zeros(1, n)
 for i = 1:n
     X_minus_i = X[[1:i-1;i+1:end], :]
     sigma_minus_i = A[[1:i-1;i+1:end], [1:i-1;i+1:end]]
-     println("actual subdet $i: ", det(X_minus_i'*(sigma_minus_i\X_minus_i)))
+    vals[i] = det(X_minus_i'*(sigma_minus_i\X_minus_i))
+    #println("actual subdet $i: ", det(X_minus_i'*(sigma_minus_i\X_minus_i)))
 end
 for i =1:n
     subdet =  det_XΣX_loocv(X, cholA, detA, i)
-    println("fast subdet $i: ", subdet)
+    vals1[i] = subdet
+    #println("fast subdet $i: ", subdet)
 end
+
+@testset "det_XΣX_loocv" begin
+    @test norm(vals-vals1)<1e-10
+end
+
+#####
+##### test logdet_loocv_IC
+#####
+#println("test logdet_loocv_IC...")
+n=5
+A = rand(n, n); A = A'*A + UniformScaling(4.0)
+B = copy(A)
+IC = incremental_cholesky!(B, n) 
+#IC = cholesky(A)
+vals = zeros(1, n)
+for i=1:n
+    sublogdet = log(det(A[[1:i-1;i+1:end], [1:i-1;i+1:end]])) 
+    vals[i] = sublogdet
+    #println("actual sublogdets $i: ", sublogdet)
+end
+vals1 = zeros(1, n)
+logdet_original = log(det(IC))
+for i = 1:n
+    sublogdet = logdet_loocv_IC(IC, logdet_original, i)
+    vals1[i] = sublogdet
+    #println("fast sublogdet $i: ", sublogdet)
+end
+@testset "logdet_loocv_IC" begin
+    @test norm(vals-vals1)<1e-10
+end
+
 
 #####
 ##### test logdet_loocv
 #####
+#println("test logdet_loocv...")
 n=5
-A = rand(n, n); A = A'*A + UniformScaling(1.0)
-#IC = incremental_cholesky!(A, n-1) #make it n-1 just for kicks
-IC = cholesky(A)
+#A = rand(n, n); A = A'*A + UniformScaling(1.0)
+#println("A: ", A)
+val = zeros(1, n)
+chol = cholesky(A)
 for i=1:n
     sublogdet = log(det(A[[1:i-1;i+1:end], [1:i-1;i+1:end]])) 
-    println("actual sublogdets $i: ", sublogdet)
+    vals[i] = sublogdet
+    #println("actual sublogdets $i: ", sublogdet)
 end
 
-logdet_original = log(det(IC))
+val1 =zeros(1, n)
+logdet_original = log(det(chol))
 for i = 1:n
-    sublogdet = logdet_loocv(IC, logdet_original, i)
-    println("fast sublogdet $i: ", sublogdet)
+    sublogdet = logdet_loocv(chol, logdet_original, i)
+    vals1[i] = sublogdet
+    #println("fast sublogdet $i: ", sublogdet)
+end
+
+@testset "logdet_loocv" begin
+    @test norm(vals-vals1)<1e-10
 end
 
 #####
 ##### test logdet_XΣX_loocv
 #####
-println("Running test logdet_XΣX_loocv...")
+#println("Running test logdet_XΣX_loocv...")
 n = 30
 m = 10
 X = rand(n, m)
@@ -95,3 +151,45 @@ for i =1:n
 end
 end
 println("norm of errors: ", norm(vals-vals1))
+
+@testset "logdet_XΣX_loocv" begin
+    @test norm(vals-vals1)<1e-10
+end
+
+#####
+##### test logdet_XΣX_loocv_IC
+#####
+#println("Running test logdet_XΣX_loocv_IC...")
+n = 30
+m = 10
+X = rand(n, m)
+#A = (w = rand(n, n);w'*w + UniformScaling(5))
+B = copy(A) #from previous
+cholA = incremental_cholesky!(B, size(A)[1])
+logdetA = logdet(cholA)
+bigmat = [A X; X' zeros(m, m)]
+#for i = 1:n
+#    println("actual subdet $i: ", det(bigmat[[1:i-1;i+1:end], [1:i-1;i+1:end]]))
+#end
+vals = zeros(1, n)
+@time begin
+for i = 1:n
+    X_minus_i = X[[1:i-1;i+1:end], :]
+    sigma_minus_i = A[[1:i-1;i+1:end], [1:i-1;i+1:end]]
+    vals[i] = log(abs(det(X_minus_i'*(sigma_minus_i\X_minus_i))))
+    #println("actual sublogdet $i: ", log(abs(det(X_minus_i'*(sigma_minus_i\X_minus_i)))))
+end
+end
+vals1 = zeros(1, n)
+@time begin
+for i =1:n
+    sublogdet =  logdet_XΣX_loocv_IC(X, cholA, logdetA, i)
+    vals1[i] = sublogdet
+    #println("fast sublogdet $i: ", sublogdet)
+end
+end
+#println("norm of errors: ", norm(vals-vals1))
+
+@testset "logdet_XΣX_loocv_IC" begin
+    @test norm(vals-vals1)<1e-10
+end
